e94f0842b04f4c72a4c619525eed1aadc7e36b99
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-02 06:56:13 -0300

    [update] TDD: Testing another scenarios
    Adding some other tests after reading the readme properly, other requirements should now be covered

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -11,22 +11,49 @@ Chai.use(chaiHttp);
 
 describe('From the /login endpoint', () => {
   describe('When a POST request is sent', () => {
-    before(() => {
-      Sinon.stub(User, 'findOne').resolves({ ...Mock.Users[0] } as User)
-    });
-    after(() => {
-      Sinon.restore();
+    describe('And succeeds', () => {
+      before(() => {
+        Sinon.stub(User, 'findOne').resolves({ ...Mock.Users[0] } as User)
+      });
+      after(() => {
+        Sinon.restore();
+      });
+  
+      it('should return a 200 status and a token', async () => {
+        const response = await Chai.request(app).post('/login').send(Mock.Login);
+        Chai.expect(response.status).to.equal(200);
+        Chai.expect(response.body.token).to.exist;
+      });
     });
 
-    it('should return a 200 status and a token when successful', async () => {
-      const response = await Chai.request(app).post('/login').send(Mock.Login);
-      Chai.expect(response.status).to.equal(200);
-      Chai.expect(response.body).to.deep.equal(Mock.Token);
-    });
+    describe('And fails', () => {
+      before(() => {
+        Sinon.stub(User, 'findOne').resolves(undefined)
+      });
+      after(() => {
+        Sinon.restore();
+      });
 
-    it('should return a 400 status when failed', async () => {
-      const response = await Chai.request(app).post('/login').send(Mock.WrongLogin);
-      Chai.expect(response.status).to.equal(400);
+      it(`should return a 400 status and missing 
+      fields message when missing data`, async () => {
+        const response = await Chai.request(app).post('/login').send(Mock.MissingLogin);
+        Chai.expect(response.status).to.equal(400);
+        Chai.expect(response.body.message).to.equal('All fields must be filled');
+      });
+  
+      it(`should return a 401 status and incorrect 
+      fields message when receiving wrong email`, async () => {
+        const response = await Chai.request(app).post('/login').send(Mock.WrongEmail);
+        Chai.expect(response.status).to.equal(401);
+        Chai.expect(response.body.message).to.equal('Incorrect email or password');
+      });
+  
+      it(`should return a 401 status and incorrect 
+      fields message when receiving wrong password`, async () => {
+        const response = await Chai.request(app).post('/login').send(Mock.WrongPassword);
+        Chai.expect(response.status).to.equal(401);
+        Chai.expect(response.body.message).to.equal('Incorrect email or password');
+      });
     });
   });
 });
