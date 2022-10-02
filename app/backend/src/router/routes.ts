db7c0f0bd835ee9e1c027396b62ffe9f94f0012c
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-02 07:13:13 -0300

    [feat] POST Login endpoint
    Adding a POST method on the router, to the /login endpoint, calling the login function on the Users controller class

--- app/backend/src/router/routes.ts
+++ app/backend/src/router/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import UsersController from '../msc/controllers/users.controllers';
+import LoginValidation from '../msc/middleware/login.validation.middleware';
 
 const routes = Router();
+
+routes.post('/login', LoginValidation.validate, UsersController.login);
 
 export default routes;
