cf3eae63670fbe432176a3b99a0b6444cad9f802
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-04 00:42:18 -0300

    [feat] GET loginValidate endpoint
    New endpoint on routes file to access the loginValidate middleware and controller.

--- app/backend/src/router/routes.ts
+++ app/backend/src/router/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import RoleValidation from '../msc/middleware/role.validation.middleware';
 import UsersController from '../msc/controllers/users.controllers';
 import LoginValidation from '../msc/middleware/login.validation.middleware';
 
 const routes = Router();
 
 routes.post('/login', LoginValidation.validate, UsersController.login);
+routes.get('/login/validate', RoleValidation.validate, UsersController.loginValidate);
 
 export default routes;
