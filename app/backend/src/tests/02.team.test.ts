13c5c17049e96ee9d625036ada554b9f2cdd9a85
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-04 18:02:33 -0300

    [feat] TDD: GET Team by ID
    New test scenario added to contemplate the findByPk on the /teams/:id method

--- app/backend/src/tests/02.team.test.ts
+++ app/backend/src/tests/02.team.test.ts
@@ -43,3 +43,47 @@ describe('From the /teams endpoint', () => {
   });
 });
 
+describe('From the /teams/:id endpoint', () => {
+  describe('When a GET request is sent', () => {
+    describe('And succeeds', () => {
+      before(() => {
+        Sinon.stub(Team, 'findByPk').resolves(Mock.Teams[0] as Team)
+      });
+      after(() => {
+        Sinon.restore();
+      });
+  
+      it('should return a 200 status and the searched team', async () => {
+        const response = await Chai.request(app).get('/teams/1');
+        Chai.expect(response.status).to.equal(200);
+        Chai.expect(response.body).to.deep.equal(Mock.Teams[0]);
+      });
+    });
+
+    describe('And fails', () => {
+      before(() => {
+        Sinon.stub(Team, 'findAll').resolves(undefined)
+      });
+      after(() => {
+        Sinon.restore();
+      });
+  
+      it(`should return a 406 status and invalid 
+      id message when id isn't a number`, async () => {
+        const response = await Chai.request(app).get('/teams');
+        Chai.expect(response.status).to.equal(406);
+        Chai.expect(response.body.message).to
+          .equal(`The id you're trying to access are not valid`);
+      });
+
+      it(`should return a 404 status and wrong 
+      id message when team isn't found`, async () => {
+        const response = await Chai.request(app).get('/teams');
+        Chai.expect(response.status).to.equal(404);
+        Chai.expect(response.body.message).to
+          .equal(`No team matches the provided id`);
+      });
+    });
+  });
+});
+
