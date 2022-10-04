04fa7daf50c5c7e22df92893ad7dec498f3fb24e
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-04 00:16:30 -0300

    [feat] TDD: GET login validate tests
    Adding new tests to the /login/validate endpoint, which shall be a GET method that uses the token to verify the user and return their role

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -57,3 +57,47 @@ describe('From the /login endpoint', () => {
     });
   });
 });
+
+describe('From the /login/validate endpoint', () => {
+  describe('When a GET request is sent', () => {
+    describe('And succeeds', () => {
+      before(() => {
+        Sinon.stub(User, 'findByPk').resolves({ ...Mock.Users[0] } as User)
+      });
+      after(() => {
+        Sinon.restore();
+      });
+  
+      it('should return a 200 status and the users role', async () => {
+        const response = await Chai.request(app).post('/login/validate').send({ token: Mock.Token });
+        Chai.expect(response.status).to.equal(200);
+        Chai.expect(response.body.role).to.exist;
+      });
+    });
+
+    describe('And fails', () => {
+      before(() => {
+        Sinon.stub(User, 'findByPk').resolves(undefined)
+      });
+      after(() => {
+        Sinon.restore();
+      });
+
+      it(`should return a 400 status and missing 
+      fields message when missing data`, async () => {
+        const response = await Chai.request(app).post('/login/validate').send();
+        Chai.expect(response.status).to.equal(400);
+        Chai.expect(response.body.role).not.to.exist;
+        Chai.expect(response.body.message).to.equal('Provide a token');
+      });
+  
+      it(`should return a 401 status and incorrect 
+      field message when receiving invalid token`, async () => {
+        const response = await Chai.request(app).post('/login').send({ token: '9999' });
+        Chai.expect(response.status).to.equal(401);
+        Chai.expect(response.body.role).to.exist;
+        Chai.expect(response.body.message).to.equal('The token is not valid');
+      });
+    });
+  });
+});
