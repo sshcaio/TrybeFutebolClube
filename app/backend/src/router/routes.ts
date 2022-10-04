ba4d89f8f25bebf6849a86d56d96b3dcf8eab7a8
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-04 19:54:37 -0300

    [feat] Teams endpoints
    Two new GET methods added, on /teams and /teams/:id endpoints, calling their respective Teams Controllers class methods

--- app/backend/src/router/routes.ts
+++ app/backend/src/router/routes.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 import RoleValidation from '../msc/middleware/role.validation.middleware';
 import UsersController from '../msc/controllers/users.controllers';
 import LoginValidation from '../msc/middleware/login.validation.middleware';
+import TeamsController from '../msc/controllers/team.controllers';
+import IdValidation from '../msc/middleware/id.validation.middleware';
 
 const routes = Router();
 
 routes.post('/login', LoginValidation.validate, UsersController.login);
 routes.get('/login/validate', RoleValidation.validate, UsersController.loginValidate);
+routes.get('/teams', TeamsController.getTeams);
+routes.get('/teams/:id', IdValidation.validate, TeamsController.getTeamById);
 
 export default routes;
