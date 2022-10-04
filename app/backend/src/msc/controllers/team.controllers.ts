cc6f6a94325abe8b49f81b86b2dc1420511ce281
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-04 19:36:00 -0300

    [feat] Teams Controllers
    New class to hold the /teams controllers methods

---
+++ app/backend/src/msc/controllers/team.controllers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import TeamsService from '../services/teams.services';
+
+class TeamsController {
+  static async getTeams(_request: Request, response: Response): Promise<Response> {
+    const result = await TeamsService.getTeams();
+    return response.status(200).json(result);
+  }
+
+  static async loginValidate(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const result = await TeamsService.getTeamById(+id);
+    return response.status(200).json(result);
+  }
+}
+
+export default TeamsController;
