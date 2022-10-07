a064340d4043a388a74794987328c247597b313f
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-07 19:44:41 -0300

    [update] Matches controllers
    Updating the method that finds all matches, adding an exception if a query string is sent on the request

--- app/backend/src/msc/controllers/matches.controllers.ts
+++ app/backend/src/msc/controllers/matches.controllers.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from 'express';
-import MatchesService from '../services/matches.services';
+import HttpException from '../../shared/http.exception';
+import MatchesServices from '../services/matches.services';
 
 class MatchesController {
-  static async getMatches(_request: Request, response: Response): Promise<Response> {
-    const result = await MatchesService.getMatches();
-    return response.status(200).json(result);
+  static async getMatches(request: Request, response: Response): Promise<Response> {
+    if (!request.query.inProgress) {
+      const result = await MatchesServices.getMatches();
+      return response.status(200).json(result);
+    }
+
+    const { inProgress } = request.query;
+    
+    let progress = true;
+    switch (inProgress) {
+      case 'true':
+        progress = true;
+        break;
+
+      case 'false':
+        progress = false;
+        break;
+    
+      default:
+        break;
+    }
+
+    const matches = await MatchesServices.getFilteredMatches(progress);
+    return response.status(200).json(matches);
   }
 }
 
