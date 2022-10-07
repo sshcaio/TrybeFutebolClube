cd05e3d5faac5064fdb597b9bfbf6958151e8ba0
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-07 19:21:03 -0300

    [feat] Matches endpoint
    New /matches endpoint, calling the findAll method from the controller with a GET

--- app/backend/src/router/routes.ts
+++ app/backend/src/router/routes.ts
@@ -3,6 +3,7 @@ import RoleValidation from '../msc/middleware/role.validation.middleware';
 import UsersController from '../msc/controllers/users.controllers';
 import LoginValidation from '../msc/middleware/login.validation.middleware';
 import TeamsController from '../msc/controllers/teams.controllers';
+import MatchesController from '../msc/controllers/matches.controllers';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.post('/login', LoginValidation.validate, UsersController.login);
 routes.get('/login/validate', RoleValidation.validate, UsersController.loginValidate);
 routes.get('/teams', TeamsController.getTeams);
 routes.get('/teams/:id', TeamsController.getTeamById);
+routes.get('/matches', MatchesController.getMatches);
 
 export default routes;
