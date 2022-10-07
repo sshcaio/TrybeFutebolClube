c624d299a359506c423b786b1deb48a6ef9b9774
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-07 19:44:41 -0300

    [feat] Matches services
    New method on the Matches services class, that catches the query string and filters the result of the findAll

--- app/backend/src/msc/services/matches.services.ts
+++ app/backend/src/msc/services/matches.services.ts
@@ -2,12 +2,30 @@ import Team from '../../database/models/TeamModel';
 import Match from '../../database/models/MatchModel';
 import HttpException from '../../shared/http.exception';
 
-class TeamsService {
+class MatchesServices {
   static async getMatches(): Promise<Match[]> {
-    const matches: Match[] = await Match.findAll({ include: [
-      { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
-      { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
-    ] });
+    const matches: Match[] = await Match.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+
+    if (!matches) {
+      throw new HttpException(500, 'Internal server error');
+    }
+
+    return matches;
+  }
+
+  static async getFilteredMatches(progress: boolean): Promise<Match[]> {
+    const matches: Match[] = await Match.findAll({
+      where: { inProgress: progress },
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
 
     if (!matches) {
       throw new HttpException(500, 'Internal server error');
@@ -17,4 +35,4 @@ class TeamsService {
   }
 }
 
-export default TeamsService;
+export default MatchesServices;
