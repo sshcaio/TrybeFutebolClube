c0bf6b19c7f8bb7e5bdc2b8f4efa0b16c04c1765
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-07 20:50:39 -0300

    [feat] POST Matches
    New endpoint to a POST method on the /matches url

--- app/backend/src/router/routes.ts
+++ app/backend/src/router/routes.ts
@@ -4,6 +4,8 @@ import UsersController from '../msc/controllers/users.controllers';
 import LoginValidation from '../msc/middleware/login.validation.middleware';
 import TeamsController from '../msc/controllers/teams.controllers';
 import MatchesController from '../msc/controllers/matches.controllers';
+import TokenValidation from '../msc/middleware/token.validation.middleware';
+import NewMatchValidation from '../msc/middleware/newmatch.validation.middleware';
 
 const routes = Router();
 
@@ -12,5 +14,7 @@ routes.get('/login/validate', RoleValidation.validate, UsersController.loginVali
 routes.get('/teams', TeamsController.getTeams);
 routes.get('/teams/:id', TeamsController.getTeamById);
 routes.get('/matches', MatchesController.getMatches);
+routes.post('/matches', TokenValidation.validate,
+  NewMatchValidation.validate, MatchesController.postMatch);
 
 export default routes;
